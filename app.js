cf09b49db03f75935a75e59ec46769a9fd612ec9
Patrick White <byelipk@kso2.local>
2017-09-04 11:03:46 -0400

    Debug in dev

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 var appServer = require('./index');
-appServer.start({ port: process.env.PORT || 3000, debug: false });
+appServer.start({
+  port: process.env.PORT || 3000,
+  debug: process.env.NODE_ENV === 'development'
+});
